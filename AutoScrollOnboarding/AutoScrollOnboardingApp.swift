99616b5e6f63e10ab1dcf4bc780ab008e83b9ddb
Raphael Cerqueira <raphaelfc@gmail.com>
2021-06-23 18:41:28 -0300

    Initial Commit

---
+++ AutoScrollOnboarding/AutoScrollOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollOnboardingApp.swift
+//  AutoScrollOnboarding
+//
+//  Created by Raphael Cerqueira on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
